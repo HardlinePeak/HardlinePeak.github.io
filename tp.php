33d95e7ccae980550d3ec3141fb8bc74426671c4
HardlinePeak <79711037+HardlinePeak@users.noreply.github.com>
2024-09-25 00:15:51 +0300

    Delete tp.php

--- tp.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!isset($_REQUEST["url"])) {
-    echo '<form method="post"><input name="url" type="text"><input type="submit" value="Open"></form>';
-    exit;
-}
-echo file_get_contents($_REQUEST["url"]);
-?>
